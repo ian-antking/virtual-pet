540c180068eba6a1da3218ef8dde8292f9aa21ad
Ian King <ian.antking@gmail.com>
2018-09-03 22:22:17 +0100

    Added tests to confirm Pet.growUp increases Pet.attributes in cumulative manner.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -21,11 +21,20 @@ describe('Constructor', () => {
 });
 describe('growUP', () => {
     const pet = new Pet('Spot');
-    pet.growUp()
-    it('Increases the Pet.age by specified amount', () => {
+    pet.growUp();
+    it('Increases the Pet.age by 1', () => {
         expect(pet.age).toEqual(1);
     });
     it('Increases the Pet.hunger by 5', () => {
         expect(pet.hunger).toEqual(5);
+    });
+    it('Decreases the Pet.fitness by 3', () => {
+        expect(pet.fitness).toEqual(7);
+    });
+    it('Changes Pet attributes cumulatively', () => {
+        pet.growUp();
+        expect(pet.age).toEqual(2);
+        expect(pet.hunger).toEqual(10);
+        expect(pet.fitness).toEqual(4);
     });
 });
