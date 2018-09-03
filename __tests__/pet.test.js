61e48b96bcf33425f68f098d5697e891638f6143
Ian King <ian.antking@gmail.com>
2018-09-03 21:14:26 +0100

    Removed years parameter from Pet.growUp method. Added Pet.hunger and Pet.fitness attributes. Added tests to confirm. Added hunger increase and fitness decrease to Pet.growUp method.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,11 +12,17 @@ describe('Constructor', () => {
     it('Pet has an initial age of 0', () => {
         expect(pet.age).toEqual(0);
     });
+    it('Pet has an inital hunger of 0', () =>{
+        expect(pet.hunger).toEqual(0);
+    });
+    it('Pet has an initial fitness of 10', () =>{
+        expect(pet.fitness).toEqual(10);
+    });
 });
 describe('growUP', () => {
     const pet = new Pet('Spot');
-    pet.growUp(5)
+    pet.growUp()
     it('Increases the Pet.age by specified amount', () => {
-        expect(pet.age).toEqual(5);
+        expect(pet.age).toEqual(1);
     });
 });
