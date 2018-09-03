91cbb252bd0c5e1b3c0b811b8504a2721b5c1ba8
Ian King <ian.antking@gmail.com>
2018-09-03 20:52:57 +0100

    Added Pet.name name at time of construction.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,4 +5,8 @@ describe('Constructor', () => {
     it('Returns an object.', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('Names the pet', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+    });
 });
