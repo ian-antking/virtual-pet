600207a5b83f92c695be489c3ac8f94ccc28a6ed
Ian King <ian.antking@gmail.com>
2018-09-03 20:44:09 +0100

    Fixed 'Object' typo in pet.test.js.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -3,6 +3,6 @@ const Pet = require('../src/pet.js');
 
 describe('Constructor', () => {
     it('Returns an object.', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(object);
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
 });
