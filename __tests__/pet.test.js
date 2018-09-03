9ea051a32997e00f0942d77673de830e4e13ebc2
Ian King <ian.antking@gmail.com>
2018-09-03 21:01:13 +0100

    Added Pet.age property and tested initial age of 0.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,8 +5,11 @@ describe('Constructor', () => {
     it('Returns an object.', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    const pet = new Pet('Fido');
     it('Names the pet', () => {
-        const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
+    });
+    it('Pet has an initial age of 0', () => {
+        expect(pet.age).toEqual(0);
     });
 });
