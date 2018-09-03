829e68c0d53361d8547cf93668e2fed88639df3d
Ian King <ian.antking@gmail.com>
2018-09-03 20:41:39 +0100

    Created test files and Pet.js.

---
+++ __tests__/pet.test.js
@@ -0,0 +1,8 @@
+
+const Pet = require('../src/pet.js');
+
+describe('Constructor', () => {
+    it('Returns an object.', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(object);
+    });
+});
